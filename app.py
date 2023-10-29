f28f51a11be56e3bbb59fc1bd7bb165c3e99a467
Shraddha Jadhav <jadhavshraddha3011@gmail.com>
2023-10-29 12:03:32 +0530

    first commit

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ def main():
 
   html3 = """
       <div style="text-align:center; text-shadow: 3px 1px 2px purple;">
-        <a href="career.html" target="_blank">Click here to view all career options</a>
+        <a href="assets/career.html" target="_blank">Click here to view all career options</a>
       </div>
       """
   st.markdown(html3, unsafe_allow_html=True)
