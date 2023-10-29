b6070a5a77d6e2c31ac14cd3f1ddc51c1d5a4794
Shraddha Jadhav <jadhavshraddha3011@gmail.com>
2023-10-29 11:50:02 +0530

    first commit

--- app.py
+++ app.py
@@ -462,9 +462,11 @@ def main():
             reading_and_writing_skills,memory_capability_score, smart_or_hard_work,
             Management_or_Techinical,Interested_subjects, Interested_Type_of_Books,
             certifications, workshops, Type_of_company_want_to_settle_in, interested_career_area)
-    
-    # Provide a link to career.html using HTML anchor tag
-    st.markdown('<a href="career.html" target="_blank">Click here to view all career options</a>', unsafe_allow_html=True)
+    html3 = """
+    <a href="career.html" target="_blank">Click here to view all career options</a>
+    """
+    st.markdown(html3, unsafe_allow_html=True)
+
 if __name__=='__main__':
     main()
 
