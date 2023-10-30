5b29e152e04f49c2e1dd15051f29e44978a21e23
Shraddha Jadhav <jadhavshraddha3011@gmail.com>
2023-10-30 12:57:30 +0530

    first commit

--- app.py
+++ app.py
@@ -298,13 +298,6 @@ def main():
       """
   st.markdown(html2,unsafe_allow_html=True) #simple html
 
-  html3 = """
-      <div style="text-align:center;">
-        <a href="career.html" target="_blank">Click here to view all career options</a>
-      </div>
-      """
-  st.markdown(html3, unsafe_allow_html=True)
-
   st.sidebar.title("Your Information")
 
   Name = st.sidebar.text_input("Full Name")
@@ -471,8 +464,12 @@ def main():
             reading_and_writing_skills,memory_capability_score, smart_or_hard_work,
             Management_or_Techinical,Interested_subjects, Interested_Type_of_Books,
             certifications, workshops, Type_of_company_want_to_settle_in, interested_career_area)
-    
-
+    html3 = """
+      <div style="text-align:center;">
+        <a href="career.html" target="_blank">Click here to view all career options</a>
+      </div>
+      """
+    st.markdown(html3, unsafe_allow_html=True)
 if __name__=='__main__':
     main()
 
